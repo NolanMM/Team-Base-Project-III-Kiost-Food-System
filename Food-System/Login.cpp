70c4e2d969723bbcb3d651c00f1c73b8e67cc2dd
Le Minh Nguyen <Lnguyen3388@conestogac.on.ca>
2022-10-31 18:26:43 -0400

    fixing the revert

--- Food-System/Login.cpp
+++ Food-System/Login.cpp
@@ -32,7 +32,6 @@ ifstream MyReadFile("filename.txt");
 // Use a while loop together with the getline() function to read the file line by line
 while (getline(MyReadFile, myText)) {
 	// Output the text from the file
-	cout << myText;
 }
 
 // Close the file
